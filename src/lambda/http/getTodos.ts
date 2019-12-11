49f255f4e70fa8b93df7a08600c3cada65444fa4
tamasdinh <tamas.dinh@gmail.com>
2019-12-11 21:20:22 +0100

    feat: added dynamoDB instance and getTodos function
    getTodos currently works without filtering for userId.

--- src/lambda/http/getTodos.ts
+++ src/lambda/http/getTodos.ts
@@ -1,7 +1,23 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+  
+  console.log('Getting all todo items via DocumentClient.scan()', event)
+  
+  const response = await docClient.scan({
+    TableName: todosTable
+  }).promise()
+
+  return {
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify(response.Items)
+  }
+
 }
