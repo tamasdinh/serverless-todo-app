f105f36494a03c006949511531f2172ec96e61ee
tamasdinh <tamas.dinh@gmail.com>
2019-12-12 13:27:47 +0100

    feat: added deleteTodo

--- src/lambda/http/deleteTodo.ts
+++ src/lambda/http/deleteTodo.ts
@@ -1,10 +1,35 @@
 import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const docClient = new DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  const itemToDelete = await docClient.query({
+    TableName: todosTable,
+    KeyConditionExpression: 'todoId = :todoId',
+    ExpressionAttributeValues: {
+      ':todoId': todoId
+    }
+  }).promise()
+ 
+  await docClient.delete({
+    TableName: todosTable,
+    Key: {
+      todoId,
+      createdAt: itemToDelete.Items[0].createdAt
+    }
+  }).promise()
+  
+  return {
+    statusCode: 201,
+    headers: {},
+    body: JSON.stringify({
+      message: `Todo w/ID ${todoId} successfully deleted.`,
+      todo_deleted: itemToDelete.Items[0]
+    })
+  }
 }
